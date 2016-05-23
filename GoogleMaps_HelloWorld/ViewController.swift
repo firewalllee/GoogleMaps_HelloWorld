203e8de2895854ae447c1a33dd6ced1d6618540b
Lee Nguyen <leenguyen@Fires-MacBook-Pro.local>
2016-05-23 17:17:45 +0700

    Update new file

--- GoogleMaps_HelloWorld/ViewController.swift
+++ GoogleMaps_HelloWorld/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set up coordinates map will show on screen.
+        mapView.camera = GMSCameraPosition.cameraWithLatitude(10.8, longitude: 106.63, zoom: 7)
+        
+        // Build and return frame map view with location (0,0), and width and height of 0.
+        let mapViewControl = GMSMapView.mapWithFrame(CGRectZero, camera: mapView.camera)
+        
+        // Set up a marker show "Hello, World!" when click on it.
+        let marker = GMSMarker()
+        marker.position = mapView.camera.target
+        marker.snippet = "Hello, World!"
+        marker.appearAnimation = kGMSMarkerAnimationPop
+        marker.map = mapView
+        
+        mapView = mapViewControl
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,4 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target ”GoogleMaps_HelloWorld” do
+	pod 'GoogleMaps'
+end
